e8d65f87f5c0a09c524a2b5adc48289b2777593c
gabewillen <gabewillen@gmail.com>
2025-10-01 03:13:40 +0000

    fix: add nil checks

--- atomic.go
+++ atomic.go
@@ -5,17 +5,30 @@ import "sync/atomic"
 type AtomicValue[T any] atomic.Value
 
 func (a *AtomicValue[T]) Load() T {
+	if a == nil {
+		var v T
+		return v
+	}
 	return (*atomic.Value)(a).Load().(T)
 }
 
 func (a *AtomicValue[T]) Store(x T) {
+	if a == nil {
+		return
+	}
 	(*atomic.Value)(a).Store(x)
 }
 
 func (a *AtomicValue[T]) Swap(x T) T {
+	if a == nil {
+		return x
+	}
 	return (*atomic.Value)(a).Swap(x).(T)
 }
 
 func (a *AtomicValue[T]) CompareAndSwap(old, new T) bool {
+	if a == nil {
+		return false
+	}
 	return (*atomic.Value)(a).CompareAndSwap(old, new)
 }
